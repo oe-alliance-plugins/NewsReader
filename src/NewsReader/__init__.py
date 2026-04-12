3e4f05098be384a540e8a0b2800b792ad7db302d
Your Name <you@example.com>
2026-04-12 14:13:01 +0000

    Migrate to standalone plugin layout

--- src/NewsReader/__init__.py
+++ src/NewsReader/__init__.py
@@ -1,22 +1,24 @@
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-from gettext import bindtextdomain, dgettext, gettext
+import gettext
 
 PluginLanguageDomain = "NewsReader"
 PluginLanguagePath = "Extensions/NewsReader/locale"
 
 
 def localeInit():
-	bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
 def _(txt):
-	if dgettext(PluginLanguageDomain, txt):
-		return dgettext(PluginLanguageDomain, txt)
+	if gettext.dgettext(PluginLanguageDomain, txt):
+		return gettext.dgettext(PluginLanguageDomain, txt)
 	else:
 		print("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt))
-		return gettext(txt)
+		return gettext.gettext(txt)
 
 
 localeInit()
 language.addCallback(localeInit)
+
+__version__ = "1.1"
--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 import setup_translate
 
 pkg = 'Extensions.NewsReader'
 setup(name='enigma2-plugin-extensions-newsreader',
        version='1.0',
        description='NewsReader for reading RSS-feeds',
+       package_dir={pkg: 'NewsReader'},
        packages=[pkg],
-       package_dir={pkg: 'plugin'},
-       package_data={pkg: ['*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       data_files=[('/etc', ['plugin/data/feeds.xml'])],
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass=setup_translate.cmdclass,  # for translation
       )
